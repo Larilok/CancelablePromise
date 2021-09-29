e054fa05518504f2da1371df6152147151038f40
Larilok <michaelmarkelovv@gmail.com>
2021-09-30 02:56:37 +0300

    add super 7/11

--- cancelable-promise.js
+++ cancelable-promise.js
@@ -2,6 +2,7 @@ class CancelablePromise extends Promise {
   constructor (query) {
     if (typeof query !== 'function')
       throw new Error('Expect promise argument to be a function')
+    super((res, rej) => query(res, rej))
   }
 }
 
