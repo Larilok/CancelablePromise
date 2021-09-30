e449c730ae63ea580b629a6896d1e753e9ab42f7
Larilok <michaelmarkelovv@gmail.com>
2021-09-30 19:02:31 +0300

    10/11

--- cancelable-promise.js
+++ cancelable-promise.js
@@ -1,11 +1,12 @@
-class CancelablePromise extends Promise {
+class CancelablePromise {
   constructor (executor, chain = []) {
     if (typeof executor !== 'function')
       throw new Error('Expect promise argument to be a function')
 
-    console.log(`constructor`)
-    super(executor)
     this.isCanceled = false
+    this.promise = new Promise((res, rej) => {
+      executor(res, rej)
+    }).catch(v => console.log(v))
     this.chain = chain
     this.chain.push(this)
   }
@@ -16,65 +17,38 @@ class CancelablePromise extends Promise {
       throw new Error('Expect first argument to be a function or undefined')
 
     const newPromise = new CancelablePromise((res, rej) => {
-      super
-        .then(
-          result => {
-            if (this.isCanceled) {
-              // rej('Promise canceled')
-              throw new Error('Promise canceled!')
-            }
-            if (onFulfilled && !this.isCanceled) res(onFulfilled(result))
-            else res(result)
-          },
-          result => {
-            if (this.isCanceled) {
-              // rej('Promise canceled')
-              throw new Error('Promise canceled!')
-            }
-            if (onRejected && !this.isCanceled) res(onRejected(result))
-            else rej(result)
+      this.promise.then(
+        result => {
+          if (this.isCanceled) {
+            rej('Promise canceled')
           }
-        )
-        .catch(v => console.log(v))
+          if (onFulfilled && !this.isCanceled) res(onFulfilled(result))
+          else res(result)
+        },
+        result => {
+          if (this.isCanceled) {
+            rej('Promise canceled')
+          }
+          if (onRejected && !this.isCanceled) res(onRejected(result))
+          else rej(result)
+        }
+      )
     }, this.chain)
     return newPromise
   }
 
-  // catch (onRejected) {
-  //   console.log(`Catch`)
-  //   return this.then(undefined, onRejected)
-  // }
+  catch (onRejected) {
+    return this.promise.then(undefined, onRejected)
+  }
 
   cancel () {
-    this.chain.forEach(promise => (promise.isCanceled = true))
+    let i = 0
+    while (true) {
+      this.chain[i].isCanceled = true
+      if (this.chain[i] === this) break
+      i++
+    }
+    return this
   }
 }
 module.exports = CancelablePromise
-
-const main = async () => {
-  let value = 0
-  const promise = new CancelablePromise(resolve =>
-    setTimeout(() => resolve(1), 100)
-  ).then(v => (value = v))
-  const promiseState = await getPromiseState(promise)
-
-  function getPromiseState (promise, callback) {
-    const unique = Symbol('unique')
-    return Promise.race([promise, Promise.resolve(unique)])
-      .then(value => (value === unique ? 'pending' : 'fulfilled'))
-      .catch(() => 'rejected')
-      .then(state => {
-        callback && callback(state)
-        return state
-      })
-  }
-
-  console.log(promiseState)
-
-  setTimeout(() => promise.cancel())
-
-  promise
-    .then(v => console.log({ then: v }))
-    .catch(v => console.log({ catch: v }))
-}
-main()
